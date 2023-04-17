f620601251029cd227dbc4d99aa60c018fc3ebb9
Saloni0412 <pshashikantb@gmail.com>
2023-04-17 01:13:16 -0400

    this commit adds function to retrive 5 day data

--- asset/script.js
+++ asset/script.js
@@ -1,99 +1,72 @@
 let search = document.querySelector(".weather-search") 
-let city = document.querySelector(".weather-city")
+let searchCity = document.querySelector(".weather-city")
 
 let date = document.querySelector(".weather-date")
 let humidity = document.querySelector(".weather-indicator-humidity>.value")
 let wind = document.querySelector(".weather-indicator-wind>.value")
-let pressure = document.querySelector(".weather-indicator-pressure>.value")
 let temperature = document.querySelector(".weather-temp>.value")
-let weatherForcastItems = document.querySelector(".weather-forecast-item")
+
+let weatherForecastItems = document.querySelector(".weather-forecast-item")
+let foreDate = document.querySelector(".weather-forecast-date")
+let foreHumidity = document.querySelector(".weather-forecast-humidity>.value")
+let foreWind = document.querySelector(".weather-forecast-wind>.value")
+let forecastTemperature = document.querySelector(".weather-forecast-temp>.value")
 
 let apiKey = "b7888d02409b33f95120630fb1218d0b";
-let weatherBaseCall = "http://api.openweathermap.org/data/2.5/weather?units=metric&appid=" + apiKey;
-let forecastBaseCall = "https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=" + apiKey;
-
-// function to get current weather details
-let weatherByCityName = async (city) => {
-let FinalAPI = weatherBaseCall + "&q=" + city;
-let response = await fetch(FinalAPI);
-let weather = await response.json()
-return weather;
-}
-
-// // function to gett next 5 days detail
-// let forecastByCityName = async (city) => {
-//      let finalAPI = forecastBaseCall + "&q=" + city;
-//      let response = await fetch(finalAPI);
-//      let forecast = await response.json();
-//      return forecast;
-//  }
+let weatherBaseCall = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" + apiKey;
 
 // event listener to search for key
 search.addEventListener('keydown', async (e) => {
      if(e.keyCode === 13) {
-          let weather = await weatherByCityName(search.value);
-          updateCurrentWeather(weather);
-          console.log(weather)
+     const city = search.value;
+     if (city) {
+       // Fetch current weather data
+       const currentWeatherResponse = await fetch(
+         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+       );
+       const currentWeatherData = await currentWeatherResponse.json();
+ 
+       // Fetch 5-day forecast data
+       const forecastResponse = await fetch(
+         `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
+       );
+       const forecastData = await forecastResponse.json();
+     console.log(forecastData)
+       // post current weather data
+       searchCity.textContent = currentWeatherData.name;
+       date.textContent = dateOfWeek();
+       humidity.textContent = currentWeatherData.main.humidity
+       wind.textContent = currentWeatherData.wind.speed
+       temperature.textContent = currentWeatherData.main.temp > 0 ? 
+                                 "+" + Math.round(currentWeatherData.main.temp) :
+                                 Math.round(currentWeatherData.main.temp);
+   
+       // post 5 day weather update
+       for (let i = 0; i < weatherForecastItems.length; i++) {
+          const weatherForecastItems = weatherForecastItems[i];
+          const forecast = forecastData.list[i * 8 + 4];
+          const forecastDate = new Date(forecast.dt_txt);
+          const forecastTemp = forecast.main.temp;
+          const forecastWind = forecast.wind.speed;
+          const forecastHumidity = forecast.main.humidity;
+  
+         weatherForecastItems.querySelector(".weather-forecast-date").textContent =
+            forecastDate.toLocaleDateString();
+         weatherForecastItems.querySelector(
+            ".weather-forecast-temp .value"
+          ).textContent = `Temp: ${forecastTemp.toFixed(2)}°C`;
+         weatherForecastItems.querySelector(
+            ".weather-forecast-wind .value"
+          ).textContent = `Wind: ${forecastWind}mph`;
+         weatherForecastItems.querySelector(
+            ".weather-forecast-humidity .value"
+          ).textContent = `Humidity: ${forecastHumidity}%`;
+        }
+     
+     }
      }
 })
 
-// search.addEventListener('keydown', async (e) => {
-//      if(e.keyCode === 13) {
-//      // Call the OpenWeatherMap API and update the UI
-//      forecastByCityName(city)
-//          .then((forecast) => {
-//              updateForecast(forecast);
-//          })
-//          .catch((error) => {
-//              console.log(error);
-//          });
-//      }
-//      console.log(forecast)
-// })
-
-// function to post data on the screen after getting it
-let updateCurrentWeather = (data) => {
-    city.textContent = data.name + "," + data.sys.country;
-    date.textContent = dateOfWeek();
-    humidity.textContent = data.main.humidity
-    wind.textContent = data.wind.speed
-    temperature.textContent = data.main.temp > 0 ? 
-                              "+" + Math.round(data.main.temp) :
-                              Math.round(data.main.temp);
-
-}
-
-// let updateForecast = (data) => {
-//      // Clear the previous forecast items from the list
-//      forecastList.innerHTML = "";
- 
-//      // Set the city title
-//      // cityTitle.textContent = `${data.city.name}, ${data.city.country}`;
- 
-//      // Loop through the forecast data for the next 5 days and add a list item for each day
-//      for (let i = 0; i < 5; i++) {
-//          let forecastItem = data.list[i];
- 
-//          let date = new Date(forecastItem.dt_txt);
-//          let dayOfWeek = date.toLocaleDateString('en-EN', {"weekday": "long"});
- 
-         
-//          let temperature = `${Math.round(forecastItem.main.temp)}°C`;
-//          let description = forecastItem.weather[0].description;
- 
-//          let forecastHTML = `
-//              <li class="forecast-item">
-//                  <div class="forecast-day">${dayOfWeek}</div>
-//                  <i class="forecast-icon ${iconClass}"></i>
-//                  <div class="forecast-temperature">${temperature}</div>
-//                  <div class="forecast-description">${description}</div>
-//              </li>
-//          `;
- 
-//          forecastList.insertAdjacentHTML("beforeend", forecastHTML);
-//      }
-//  }
-// function for getting date
 let dateOfWeek = () => {
      return new Date().toLocaleDateString('en-EN', {"dateStyle": "long"})
 }
